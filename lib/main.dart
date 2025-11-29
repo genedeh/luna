c13dc6957870fee7330d1d242e8b88d4983e4ad4
genedeh <genesis.enedeh@gmail.com>
2025-11-29 03:32:59 +0100

    Add WelcomePage with dark/light mode support and notifier integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:luna/data/notifiers.dart';
+import 'package:luna/pages/welcome_page.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello Worldzz!'))),
+    return ValueListenableBuilder(
+      valueListenable: isDarkModeNotifier,
+      builder: (context, isDarkMode, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            brightness: Brightness.light,
+            primaryColor: Colors.black, // you control your primary manually
+            scaffoldBackgroundColor: Colors.white,
+            colorScheme: const ColorScheme.light(), // basic light scheme
+          ),
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            primaryColor: Colors.white,
+            scaffoldBackgroundColor: Colors.black,
+            colorScheme: const ColorScheme.dark(), // basic dark scheme
+          ),
+          themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
+          home: const WelcomePage(),
+        );
+      },
     );
   }
 }
---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:luna/data/notifiers.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: SizedBox(
+                width: double.infinity,
+                child: ValueListenableBuilder(
+                  valueListenable: isDarkModeNotifier,
+                  builder: (context, isDarkMode, child) {
+                    return Image.asset(
+                      isDarkMode
+                          ? 'assets/images/welcome_dark.png'
+                          : 'assets/images/welcome_light.jpeg',
+                      fit: BoxFit.cover,
+                    );
+                  },
+                ),
+              ),
+            ),
+
+            Container(
+              padding: EdgeInsets.all(24),
+              height: 370,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(50)),
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Luna",
+                          style: TextStyle(
+                            fontSize: 54,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(width: 12),
+                        SizedBox(
+                          width: 50,
+                          height: 50,
+                          child: ValueListenableBuilder(
+                            valueListenable: isDarkModeNotifier,
+                            builder: (context, isDarkMode, child) {
+                              return Image.asset(
+                                isDarkMode
+                                    ? 'assets/images/logo_dark.png'
+                                    : 'assets/images/logo_light.png',
+                                fit: BoxFit.contain,
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 32),
+                    Text(
+                      "Your personal note-taking app that keeps your thoughts organized and accessible anytime, anywhere.",
+                      style: TextStyle(fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 24),
+                    Column(
+                      children: [
+                        SizedBox(
+                          width: double.infinity,
+                          child: FilledButton(
+                            onPressed: () {},
+                            style: FilledButton.styleFrom(
+                              padding: EdgeInsets.symmetric(vertical: 16),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                            child: Text(
+                              "Get Started",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        SizedBox(
+                          width: double.infinity,
+                          child: TextButton(
+                            onPressed: () {},
+                            style: TextButton.styleFrom(
+                              padding: EdgeInsets.symmetric(vertical: 10),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                            child: Text(
+                              "Login",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
